3a2d3adb44387df05536250b7ca2bd4b91246503
edumendes1337 <76907135+edumendes1337@users.noreply.github.com>
2025-04-28 20:26:10 -0300

    Create pagesAdmin.jsx

---
+++ src/pagesAdmin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Admin() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Bem-vindo à Área do Administrador</h1>
+      <p>Você está logado!</p>
+    </div>
+  );
+}
+
+export default Admin;
